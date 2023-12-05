ceaff15436ca90c89f899fe6890a71affe97d4f4
Ashfiq98 <you@example.com>
2023-12-05 21:34:37 +0600

    Updated

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.post('/', (req, res) => {
 
 
 });
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is running on port 3000...');
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}..`);
 });
